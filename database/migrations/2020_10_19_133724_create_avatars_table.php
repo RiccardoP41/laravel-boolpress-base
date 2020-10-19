d22cf21f3f12a13cf55450395f8b7feb439c6e59
Riccardo Pasini <riccardo.pasini1992@gmail.com>
2020-10-19 17:12:53 +0200

    consegna fatta

---
+++ database/migrations/2020_10_19_133724_create_avatars_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAvatarsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('avatars', function (Blueprint $table) {
+            $table->id();
+            $table->string('telefono',25);
+            $table->text('avatar');
+
+            //creiamo il campo user_id
+            $table->unsignedBigInteger('user_id');
+            //facciamo la relazione con la tabella user
+            $table->foreign('user_id')
+            ->references('id')
+            ->on('users')
+            ->onDelete('cascade')
+            ->onUpdate('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('avatars');
+    }
+}
